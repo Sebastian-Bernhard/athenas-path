f03b75ede34038626ac00a413fd7bbb00030b8f6
SebastianBernhard <sebastian.bernhard@natuvion.com>
2024-12-12 08:40:42 +0000

    what I learned 1112

--- caesar/src/Lib.hs
+++ caesar/src/Lib.hs
@@ -8,3 +8,12 @@ upperAlphabet :: Alphabet
 upperAlphabet = ['A' .. 'Z']
 digits :: Alphabet
 digits = ['0' .. '9']
+
+isLower :: Char -> Bool
+isLower char = char `elem` lowerAlphabet
+isUpper :: Char -> Bool
+isUpper char = char `elem` upperAlphabet
+isDigit :: Char -> Bool
+isDigit char = char `elem` digits
+isMisc :: Char -> Bool
+isMisc char = char `notElem` lowerAlphabet ++ upperAlphabet ++ digits
