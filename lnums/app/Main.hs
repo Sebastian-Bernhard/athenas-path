96b7279e735aba8b87f1929a987095682c84f622
SebastianBernhard <sebastian.bernhard@natuvion.com>
2025-01-14 06:53:18 +0000

    what I learned 1301

--- lnums/app/Main.hs
+++ lnums/app/Main.hs
@@ -9,5 +9,27 @@ readLines filePath = do
     contents <- readFile filePath
     return (lines contents)
 
+printHelpText :: String -> IO ()
+printHelpText msg = do
+    putStrLn (msg ++ "\n")
+    progName <- getProgName
+    putStrLn ("Usage: " ++ progName ++ " <filename>")
+
+parseArguments :: [String] -> Maybe FilePath
+parseArguments [filePath] = Just filePath
+parseArguments _ = Nothing
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    cliArgs <- getArgs
+    let mFilePath = parseArguments cliArgs
+    maybe
+        (printHelpText "Missing filename")
+        ( \filePath -> do
+            fileLines <- readLines filePath
+            let numbered = numberAndIncrementNonEmptyLines fileLines
+                prettyNumbered = prettyNumberedLines
+                    PadLeft numbered
+            mapM_ putStrLn prettyNumbered
+        )
+        mFilePath
--- lnums/src/Lib.hs
+++ lnums/src/Lib.hs
@@ -1,15 +1,26 @@
-module Lib 
-    (
-        NumberedLine,
-        NumberedLines,
-        numberAllLines,
-        isEmpty
-    ) where
+module Lib
+  ( NumberedLine,
+    NumberedLines,
+    isEmpty,
+    isNotEmpty,
+    numberLines,
+    numberAllLines,
+    numberNonEmptyLines,
+    numberAndIncrementNonEmptyLines,
+    prettyNumberedLines,
+    PadMode (..),
+    pad,
+    padLeft,
+    padRight,
+  )
+where
 import Data.Char
 
 type NumberedLine = (Maybe Int, String)
 type NumberedLines = [NumberedLine]
 
+-- number all lines without case handeling any characters
+{-
 numberAllLines :: [String] -> NumberedLines
 numberAllLines lines =
     let go :: Int -> [String] -> NumberedLines
@@ -19,8 +30,9 @@ numberAllLines lines =
         go counter (line:rest) = (Just counter, line) : go (counter + 1) rest
     -- call go func, start the recursion by counter = 1 and the list of lines
     in go 1 lines
+-}
 
--- impl the func `lines` that splits a string into lines by '\n'
+-- split a string into lines by '\n'
 {-
 go is a helper function that processes the input string.
 go "" "" = [] handles the case where both the current line and the remaining text are empty.
@@ -44,6 +56,9 @@ isEmpty str =
     null str -- check if the string is empty
     || all (\s -> not (isPrint s) || isSeparator s) str
 
+isNotEmpty :: String -> Bool
+isNotEmpty str = not (isEmpty str)
+
 numberLines :: (String -> Bool) -> (String -> Bool) -> [String] -> NumberedLines
 numberLines shouldIncr shouldNumber text =
     let go :: Int -> [String] -> NumberedLines
@@ -51,8 +66,56 @@ numberLines shouldIncr shouldNumber text =
         go counter (x : xs) = 
             let mNumbering = if shouldNumber x then Just counter else Nothing
                 newCounter = if shouldIncr x then counter + 1 else counter
-            in (mNumbering, x) : go newCounter xs
+            -- construct a new list by prepending the tuple to the result of the recursive call 'go newCounter xs'
+            in (mNumbering, x) : go newCounter xs 
    in go 1 text
+
+numberAllLines :: [String] -> NumberedLines
+numberAllLines = numberLines (const True) (const True)
+
+numberNonEmptyLines :: [String] -> NumberedLines
+numberNonEmptyLines = numberLines (const True) isNotEmpty
+
+numberAndIncrementNonEmptyLines :: [String] -> NumberedLines
+numberAndIncrementNonEmptyLines = numberLines isNotEmpty isNotEmpty
+
+data PadMode = PadLeft | PadRight
+
+pad :: PadMode -> Int -> String -> String
+pad mode n str =
+  let diff = n - length str
+      padding = replicate diff ' ' -- create a string of n spaces
+   in case mode of
+        PadLeft -> padding ++ str
+        PadRight -> str ++ padding
+
+padLeft :: Int -> String -> String
+padLeft = pad PadLeft
+
+padRight :: Int -> String -> String
+padRight = pad PadRight
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] [] = []
+zip' _ [] = []
+zip' [] _ = []
+zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] [] = []
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
+zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+prettyNumberedLines :: PadMode -> NumberedLines -> [String]
+prettyNumberedLines mode lineNums =
+    let (numbers, text) = unzip lineNums
+        numberStrings = map (maybe "" show) numbers
+        maxLength = maximum (map length numberStrings)
+        paddedNumbers = map (pad mode maxLength) numberStrings
+    in zipWith (\n l -> n ++ " " ++ l) paddedNumbers text
+
+
 
 
     
